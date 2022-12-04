333e7b5f3809f23a83367ab666a0732fdeddd67d
Matthew <mrdicke22@icloud.com>
2022-12-03 19:52:13 -0600

    conversion fix

--- receiptToPoints.py
+++ receiptToPoints.py
@@ -13,6 +13,7 @@ def receiptConverter(receipt):
         if char.isalnum():
             points += 1
 
+
 # Handles converting total to point value
     if total[1] == '00':
         points += 50
@@ -26,6 +27,7 @@ def receiptConverter(receipt):
     else:
         points += 0
 
+
 # Handles converting Items to point values
     numberOfitems = len(items)
     if numberOfitems % 2 == 1:
@@ -34,6 +36,7 @@ def receiptConverter(receipt):
     else:
         pairsOfitems = int((numberOfitems) / 2)
         points += (pairsOfitems * 5)
+
 
 # Handles converting item description to points
     for item in items:
@@ -45,7 +48,7 @@ def receiptConverter(receipt):
 
 # Handle converting date to points
     militaryTime = int(time.replace(':',''))
-    if militaryTime > 1200 or militaryTime < 1400:
+    if militaryTime >= 1400 and militaryTime <= 1600:
         points += 10
 
 
@@ -53,7 +56,6 @@ def receiptConverter(receipt):
     day = int(date[2])
     if day % 2 == 1:
         points += 6
-
 
     return points
 
@@ -66,50 +68,54 @@ def receiptConverter(receipt):
 #
 #
 #
-# receipt = {
-#   "retailer": "Target",
-#   "purchaseDate": "2022-01-01",
-#   "purchaseTime": "13:01",
-#   "items": [
-#     {
-#       "shortDescription": "Mountain Dew 12PK",
-#       "price": "6.49"
-#     },{
-#       "shortDescription": "Emils Cheese Pizza",
-#       "price": "12.25"
-#     },{
-#       "shortDescription": "Knorr Creamy Chicken",
-#       "price": "1.26"
-#     },{
-#       "shortDescription": "Doritos Nacho Cheese",
-#       "price": "3.35"
-#     },{
-#       "shortDescription": "Klarbrunn 12PK 12 FL OZ",
-#       "price": "12.00"
-#     }
-#   ],
-#   "total": "35.35"
-# }
+receipt = {
+  "retailer": "Target",
+  "purchaseDate": "2022-01-01",
+  "purchaseTime": "13:01",
+  "items": [
+    {
+      "shortDescription": "Mountain Dew 12PK",
+      "price": "6.49"
+    },{
+      "shortDescription": "Emils Cheese Pizza",
+      "price": "12.25"
+    },{
+      "shortDescription": "Knorr Creamy Chicken",
+      "price": "1.26"
+    },{
+      "shortDescription": "Doritos Nacho Cheese",
+      "price": "3.35"
+    },{
+      "shortDescription": "Klarbrunn 12PK 12 FL OZ",
+      "price": "12.00"
+    }
+  ],
+  "total": "35.35"
+}
+#25
 
-# receiptTwo = {
-#   "retailer": "M&M Corner Market",
-#   "purchaseDate": "2022-03-20",
-#   "purchaseTime": "14:33",
-#   "items": [
-#     {
-#       "shortDescription": "Gatorade",
-#       "price": "2.25"
-#     },{
-#       "shortDescription": "Gatorade",
-#       "price": "2.25"
-#     },{
-#       "shortDescription": "Gatorade",
-#       "price": "2.25"
-#     },{
-#       "shortDescription": "Gatorade",
-#       "price": "2.25"
-#     }
-#   ],
-#   "total": "9.00"
-# }
+receiptTwo = {
+  "retailer": "M&M Corner Market",
+  "purchaseDate": "2022-03-20",
+  "purchaseTime": "14:33",
+  "items": [
+    {
+      "shortDescription": "Gatorade",
+      "price": "2.25"
+    },{
+      "shortDescription": "Gatorade",
+      "price": "2.25"
+    },{
+      "shortDescription": "Gatorade",
+      "price": "2.25"
+    },{
+      "shortDescription": "Gatorade",
+      "price": "2.25"
+    }
+  ],
+  "total": "9.00"
+}
+#109
 ##################################################################################################
+receiptConverter(receipt)
+# receiptConverter(receiptTwo)
